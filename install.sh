bdc91677419ee97da8c355514653352602a6fb90
Francois Njeneza <njenezamed@gmail.com>
2017-05-21 16:33:43 +0200

    resize font

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cd /usr/lib/urxvt/perl
 wget https://raw.githubusercontent.com/effigies/urxvt-perl/master/fullscreen
+wget https://raw.githubusercontent.com/simmel/urxvt-resize-font/master/resize-font

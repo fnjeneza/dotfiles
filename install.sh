20eb9ff2d23d16eb2f858c5604806630aa2d9a63
Francois Njeneza <njenezamed@gmail.com>
2017-05-21 16:28:03 +0200

    enable fullscreen

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-apt install rxvt-unicode-256color i3 tmux
+apt install rxvt-unicode-256color i3 tmux wmctrl
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+cd /usr/lib/urxvt/perl
+wget https://raw.githubusercontent.com/effigies/urxvt-perl/master/fullscreen

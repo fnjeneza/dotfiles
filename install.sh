423046960a0ad952d8388a07e2057acd7660866d
Francois Njeneza <njenezamed@gmail.com>
2019-01-07 20:20:49 +0100

    basic libraries and tools

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
+install sudo vim i3 tmux git xorg firefox-esr
 apt install rxvt-unicode-256color i3 tmux wmctrl
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-cd /usr/lib/urxvt/perl
-wget https://raw.githubusercontent.com/effigies/urxvt-perl/master/fullscreen
-wget https://raw.githubusercontent.com/simmel/urxvt-resize-font/master/resize-font
